723efd6252fa0dc1f94462d923055a7930fa468f
Subhayu Kumar Bala <subhayu.kumar@fiftyfivetech.io>
2023-01-12 22:43:41 +0530

    fix: revert aliases

--- script.sh
+++ script.sh
@@ -1,10 +1,3 @@
-alias d='sudo docker'
-alias dstop='sudo docker stop $(sudo docker ps -q -f ancestor=auto-keka)'
-alias dbuild='sudo docker build -t auto-keka . --no-cache'
-alias drun='sudo docker run -dp 5000:5000 auto-keka'
-alias dexec='sudo docker exec -it $(sudo docker ps -q -f ancestor=auto-keka) /bin/bash'
-alias dlogs='sudo docker logs $(sudo docker ps -q -f ancestor=auto-keka)'
-
-dstop
-dbuild
-drun
+sudo docker stop $(sudo docker ps -q -f ancestor=auto-keka)
+sudo docker build -t auto-keka . --no-cache
+sudo docker run -dp 5000:5000 auto-keka
