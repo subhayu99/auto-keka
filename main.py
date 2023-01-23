f9fda08db1a5c99d132a73f361a85f1f0eef815d
Subhayu Kumar Bala <subhayu.kumar@fiftyfivetech.io>
2023-01-23 11:38:15 +0530

    fix: path problems

--- main.py
+++ main.py
@@ -17,20 +17,13 @@ user = User()
 keka = Keka(user)
 
 
-@app.get("/punch/", description="`Punch In` or `Punch Out` based on last punch status")
+@app.get("/punch", description="`Punch In` or `Punch Out` based on last punch status")
 def punch_with_opposite_type(force: bool = False):
     status_code, message = keka.punch(force=force)
     return PlainTextResponse(message, status_code=status_code)
 
 
-@app.get("/punch/{punch_type}/", description="**0** for `Punch In`, **1** for `Punch Out`")
-def punch_with_given_type(punch_type: config.AllowedPunchType, force: bool = False):
-    punch_type = config.PunchType(punch_type.value)
-    status_code, message = keka.punch(punch_type, force=force)
-    return PlainTextResponse(message, status_code=status_code)
-
-
-@app.get("/punch/get_state/")
+@app.get("/punch/state")
 def get_punch_state():
     punch_status, timestamp = keka.retrieve_state()
     punch_message = config.punch_message_map[punch_status.value]
@@ -43,13 +36,20 @@ def get_punch_state():
     }
 
 
-@app.get("/token/refresh/")
+@app.get("/punch/{punch_type}", description="**0** for `Punch In`, **1** for `Punch Out`")
+def punch_with_given_type(punch_type: config.AllowedPunchType, force: bool = False):
+    punch_type = config.PunchType(punch_type.value)
+    status_code, message = keka.punch(punch_type, force=force)
+    return PlainTextResponse(message, status_code=status_code)
+
+
+@app.get("/token/refresh")
 async def refresh_token():
     keka.refresh_token(headless=True)
     return get_token_age()
 
 
-@app.get("/token/age/")
+@app.get("/token/age")
 def get_token_age():
     token_age, timestamp = keka.get_token_age(auto_load=True)
     return {
@@ -60,17 +60,17 @@ def get_token_age():
     }
 
 
-@app.get("/user/details/", response_model=ReturnUser)
+@app.get("/user/details", response_model=ReturnUser)
 def get_user():
     return user.get_user()
 
 
-@app.get("/user/keka_profile/")
+@app.get("/user/keka_profile")
 def get_keka_profile():
     return keka.get_keka_profile()
 
 
-@app.get("/user/work_time_for_date/", description="Gives total working time for a given date. Date format: `YYYY-MM-DD`")
+@app.get("/user/work_time_for_date", description="Gives total working time for a given date. Date format: `YYYY-MM-DD`")
 def get_work_time_for_date(for_date: str):
     work_time = keka.get_work_time_for_date(for_date:=date.fromisoformat(for_date))
     return {
@@ -80,12 +80,12 @@ def get_work_time_for_date(for_date: str):
     }
 
 
-@app.get("/scheduler/is_running/", response_model=bool)
+@app.get("/scheduler/is_running", response_model=bool)
 def is_scheduler_running():
     return bool([x for x in psutil.process_iter(["pid", "name"]) if x.cmdline() and "schedule.py" in x.cmdline()])
 
 
-# @app.get("/scheduler/get_logs/", response_model=list[LogModel])
+# @app.get("/scheduler/get_logs", response_model=list[LogModel])
 # def get_scheduler_logs(length: int = 100):
 #     return helpers.get_logs("scheduler.log", length)
 
